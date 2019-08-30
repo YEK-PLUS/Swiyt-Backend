8fb3e2854d342b2ad2185bad26febb05acedd086
YEK-PLUS <admin@f4med.com>
2019-08-31 01:43:37 +0300

    delete hbs view ,just api

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  res.send(404, 'not found').end();
 });
 
 module.exports = app;
