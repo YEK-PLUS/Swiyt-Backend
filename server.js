c4fdd21dcfeb61c19da1c078c94d50726a41c3cc
YEK-PLUS <admin@f4med.com>
2019-08-31 16:45:12 +0300

    resetting server

---
+++ server.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const http = require('http');
+global.key = require('./keys');
+const app = require('./main');
+
+const port = (process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
