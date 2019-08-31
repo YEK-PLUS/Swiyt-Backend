0e225e37d70a61298617eadcb788577188a80be6
YEK-PLUS <admin@f4med.com>
2019-08-31 18:43:03 +0300

    adding routes main file

--- requirements/index.js
+++ requirements/index.js
@@ -1,5 +1,7 @@
 import _express from './express';
 import _passport from './passport';
+import _routes from '../routes';
 
 export const express = _express;
 export const passport = _passport;
+export const routes = _routes;
