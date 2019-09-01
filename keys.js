c9c6d7e597e57d45d4f7c40a6c67e67532a08f0c
YEK-PLUS <admin@f4med.com>
2019-09-01 19:12:39 +0300

    change whitelist

--- keys.js
+++ keys.js
@@ -9,6 +9,8 @@ module.exports = {
     dialect: 'mysql',
   },
   whitelist: [
-    '/auth/token',
+    'http://localhost:3000',
+    'http://192.168.1.105:3000',
+    'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop',
   ],
 };
