7f94124dc07e86f02b4b3a9cf18b16af93bd6e4d
YEK-PLUS <admin@f4med.com>
2019-09-01 04:10:54 +0300

    adding auth endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import mainRouter from './main';
+import authRouter from './auth';
 
 export const initializeRoutes = (app) => {
   app.use('/main', mainRouter);
+  app.use('/auth', authRouter);
 };
 export const initializeError = (app) => {
   app.use((req, res) => {
