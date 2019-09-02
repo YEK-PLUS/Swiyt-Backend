b96077b0f2e1e0c68f83e875dead990b6b07d310
YEK-PLUS <admin@f4med.com>
2019-09-03 01:07:09 +0300

    add new relationship

--- models/model/index.js
+++ models/model/index.js
@@ -1,7 +1,12 @@
 import User from './user';
 import UserDetails from './userDetails';
+import Images from './images';
+import Cdn from './cdn';
 
 UserDetails.belongsTo(User, { foreignKey: 'uid' });
 User.belongsTo(UserDetails, { foreignKey: 'uid' });
+
+User.belongsTo(Images, { foreignKey: 'avatar_uid' });
+Images.belongsTo(Cdn, { foreignKey: 'uid' });
 
 module.exports = { User, UserDetails };
