b5c3f3c9921157e91427ce7eb6d06d8e46d12b3f
YEK-PLUS <admin@f4med.com>
2019-09-02 03:08:37 +0300

    adding userDetails Object

---
+++ models/model/userDetails.js
@@ -0,0 +1,35 @@
+import Sequelize from 'sequelize';
+
+module.exports = sequelize.define('user_details', {
+  uid: { type: Sequelize.CHAR, primaryKey: true },
+
+  telephone: Sequelize.STRING,
+  telephone_verified: Sequelize.BOOLEAN,
+  telephone_code: Sequelize.STRING,
+
+  mail: Sequelize.STRING,
+  mail_verified: Sequelize.BOOLEAN,
+  mail_code: Sequelize.STRING,
+
+  firstname: Sequelize.STRING,
+  lastname: Sequelize.STRING,
+
+  biography: Sequelize.TEXT,
+  summary: Sequelize.TEXT,
+
+  location: Sequelize.STRING,
+  title: Sequelize.STRING,
+
+  links: Sequelize.STRING,
+  referance: Sequelize.STRING,
+  portfolio: Sequelize.STRING,
+  certificates: Sequelize.STRING,
+  experience: Sequelize.STRING,
+  education: Sequelize.STRING,
+
+  deleted_at: Sequelize.DATE,
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE,
+}, {
+  underscored: true,
+});
