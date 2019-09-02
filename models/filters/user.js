af9800b00a126f47a70422cd72f9f63a09d3bfcf
YEK-PLUS <admin@f4med.com>
2019-09-03 01:05:33 +0300

    update filtering

--- models/filters/user.js
+++ models/filters/user.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
+
 export const JustMe = (userModel) => {
-  let user = userModel.toJSON();
-  let filteredUser = _.pick(user,[
-    "uid","username","realname"
+  const user = userModel.toJSON();
+  const filteredUser = _.pick(user, [
+    'uid', 'username', 'realname',
+  ]);
+  filteredUser.image = _.pick(user.image, [
+    'uid',
+    'cdn_uid',
+  ]);
+  filteredUser.image.cdn = _.pick(user.image.cdn_datum, [
+    'uid',
+    'url',
   ]);
   return filteredUser;
-}
+};
