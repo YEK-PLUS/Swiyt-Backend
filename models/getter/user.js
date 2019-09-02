29279b474b3763797537f0862971ec1fc7633e98
YEK-PLUS <admin@f4med.com>
2019-09-03 01:06:47 +0300

    add relationship

--- models/getter/user.js
+++ models/getter/user.js
@@ -1,18 +1,36 @@
 import User from '../model/user';
+import Images from '../model/images';
+import Cdn from '../model/cdn';
+
+export const UserIncludes = {
+  include: [
+    {
+      model: Images,
+      include: [
+        {
+          model: Cdn,
+        },
+      ],
+    },
+  ],
+};
 
 export const JustUser = (username, password) => User.findOne({
   where: {
     username,
     password,
   },
+  ...UserIncludes,
 });
 export const JustUserWithUid = (uid) => User.findOne({
   where: {
     uid,
   },
+  ...UserIncludes,
 });
 export const JustUserWithUserName = (username) => User.findOne({
   where: {
     username,
   },
+  ...UserIncludes,
 });
