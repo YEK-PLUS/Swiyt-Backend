126fdfb87d0c7ac86dabe5810cfd4e0446ab6633
YEK-PLUS <admin@f4med.com>
2019-09-04 01:15:45 +0300

    objects relationship re-coded

--- models/model/index.js
+++ models/model/index.js
@@ -3,10 +3,11 @@ import UserDetails from './userDetails';
 import Images from './images';
 import Cdn from './cdn';
 
-UserDetails.belongsTo(User, { foreignKey: 'uid' });
 User.belongsTo(UserDetails, { foreignKey: 'uid' });
 
 User.belongsTo(Images, { foreignKey: 'avatar_uid' });
 Images.belongsTo(Cdn, { foreignKey: 'uid' });
 
+UserDetails.belongsTo(Images, { foreignKey: 'picture_uid', as: 'picture' });
+UserDetails.belongsTo(Images, { foreignKey: 'banner_uid', as: 'banner' });
 module.exports = { User, UserDetails };
