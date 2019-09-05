ee4ba43c715cfe11cc21f7ea2ecaffebd1a8f8cb
YEK-PLUS <admin@f4med.com>
2019-09-05 21:49:19 +0300

    updated for heroku

--- keys.js
+++ keys.js
@@ -2,15 +2,17 @@ module.exports = {
   sessionSecret: 'keyboard cat',
   JWTkey: 'secret',
   database: {
-    dbname: 'swiyt',
-    username: 'root',
-    password: '',
-    host: 'localhost',
+    dbname: 'heroku_b543e43b5602ad7',
+    username: 'b531d6fd58abaf',
+    password: '1d7e0832',
+    host: 'eu-cdbr-west-02.cleardb.net',
     dialect: 'mysql',
   },
   whitelist: [
     'http://localhost:3000',
+    'http://localhost:8080',
     'http://192.168.1.105:3000',
+    'http://192.168.1.105:8080',
     'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop',
   ],
   returns: {
