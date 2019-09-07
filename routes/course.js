e897c878ac87c636d71b1825bee427d9799225ed
YEK-PLUS <admin@f4med.com>
2019-09-07 23:27:53 +0300

    added get subscription route

--- routes/course.js
+++ routes/course.js
@@ -5,7 +5,7 @@ import auth from '../middlewares/auth';
 import Models from '../models';
 
 const {
-  PopulerCourses,FilterCourse
+  PopulerCourses,Subscriptions,FilterCourse,FilterSubscription
 } = Models;
 const router = Router();
 
@@ -13,6 +13,13 @@ router.post('/populer', (req, res) => {
   return PopulerCourses().then(courses => {
     let a = [];
     courses.map(b=>a.push(FilterCourse(b)));
+    return res.status(200).send(a).end();
+  });
+});
+router.post('/subscriptions',auth, (req, res) => {
+  return Subscriptions(req.user.uid).then(subscriptions => {
+    let a = [];
+    subscriptions.map(b=>a.push(FilterSubscription(b)));
     return res.status(200).send(a).end();
   });
 });
