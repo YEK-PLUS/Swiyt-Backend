86037551cddcf64753005bca6b13a291c40dc07a
YEK-PLUS <admin@f4med.com>
2019-09-07 23:26:32 +0300

    added subscription getter

--- models/getter/course.js
+++ models/getter/course.js
@@ -1,8 +1,12 @@
 import Course from '../model/course';
+import Subscription from '../model/subscription';
 import * as Includes from '../includes';
 
-const { IncludeCourse } = Includes;
+const { IncludeCourse,IncludeSubscription } = Includes;
 export const PopulerCourses = () => Course.findAll({
   limit: 5,
   include: IncludeCourse,
+});
+export const Subscriptions = (user_uid) => Subscription.findAll({
+  include: IncludeSubscription
 });
