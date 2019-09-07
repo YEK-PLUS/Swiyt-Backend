d469fc0964e802e97a3734a9567d97dfd6702418
YEK-PLUS <admin@f4med.com>
2019-09-07 23:27:18 +0300

    update relationship

--- models/model/index.js
+++ models/model/index.js
@@ -3,10 +3,11 @@ import UserDetails from './userDetails';
 import Images from './images';
 import Cdn from './cdn';
 import Course from './course';
+import Subscription from './subscription';
 User.belongsTo(UserDetails, { foreignKey: 'uid' });
 
 User.belongsTo(Images, { foreignKey: 'avatar_uid' });
-Images.belongsTo(Cdn, { foreignKey: 'uid' });
+Images.belongsTo(Cdn, { foreignKey: 'cdn_uid' });
 
 UserDetails.belongsTo(Images, { foreignKey: 'picture_uid', as: 'picture' });
 UserDetails.belongsTo(Images, { foreignKey: 'banner_uid', as: 'banner' });
@@ -16,4 +17,7 @@ Course.belongsTo(Images, { foreignKey: 'thub_uid', as: 'thub' });
 
 Course.belongsTo(User, { foreignKey: 'user_uid', as: 'admin' });
 
-module.exports = { User, UserDetails,Course };
+Subscription.belongsTo(Course,{ foreignKey: 'lesson_uid'});
+
+
+module.exports = { User, UserDetails,Course,Subscription };
