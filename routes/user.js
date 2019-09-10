7d6d00d39090bfec13542fa487acc79bd6b8e817
YEK-PLUS <admin@f4med.com>
2019-09-10 22:24:43 +0300

    include new filter and getter

--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,18 @@ import auth from '../middlewares/auth';
 import Models from '../models';
 
 const {
-  JustUserDetailsAndCourseWithUserName,FilterUserAndUserDetailsAndUserCourse,JustPopulerUser,FilterUser
+  JustUserDetailsAndCourseWithUserName,FilterUserAndUserDetailsAndUserCourse,JustPopulerUserAndCourses,FilterUserAndUserCourse
 } = Models;
 const router = Router();
 
 router.post('/userDetails', (req, res) => JustUserDetailsAndCourseWithUserName(req.body.username).then((userDetail) => {
   res.status(200).send(FilterUserAndUserDetailsAndUserCourse(userDetail)).end();
 }));
-router.post('/populer', (req, res) => JustPopulerUser().then((usersModel) => {
+router.post('/populer', (req, res) => JustPopulerUserAndCourses().then((usersModel) => {
   let users = [];
-  usersModel.map(user => {
-    return users.push(FilterUser(user));
-  });
+  usersModel.rows.map(user => {
+    return users.push(FilterUserAndUserCourse(user));
+  })
   res.status(200).send((users)).end();
 }));
 
