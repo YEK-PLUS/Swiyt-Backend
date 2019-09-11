6699a805ee3f3f306971f6bdac459a03e1936d72
YEK-PLUS <admin@f4med.com>
2019-09-11 23:20:37 +0300

    getter include

--- models/getter/index.js
+++ models/getter/index.js
@@ -1,4 +1,5 @@
 import * as User from './user';
 import * as Lesson from './course';
+import * as Reference from './reference';
 
-module.exports = { ...User, ...Lesson };
+module.exports = { ...User, ...Lesson,...Reference };
