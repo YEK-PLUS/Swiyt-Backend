2fb88f96debb240d107b4340cc01d43c5effc558
YEK-PLUS <admin@f4med.com>
2019-09-11 20:40:00 +0300

    add new route -> get all populer users

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,19 @@ import auth from '../middlewares/auth';
 import Models from '../models';
 
 const {
-  JustUserDetailsAndCourseWithUserName,FilterUserAndUserDetailsAndUserCourse,JustPopulerUserAndCourses,FilterUserAndUserCourse
+  JustUserDetailsAndCourseWithUserName,FilterUserAndUserDetailsAndUserCourse,JustAllPopulerUserAndCourses,JustPopulerUserAndCourses,FilterUserAndUserCourse
 } = Models;
 const router = Router();
 
 router.post('/userDetails', (req, res) => JustUserDetailsAndCourseWithUserName(req.body.username).then((userDetail) => {
   res.status(200).send(FilterUserAndUserDetailsAndUserCourse(userDetail)).end();
+}));
+router.post('/populer/all', (req, res) => JustAllPopulerUserAndCourses().then((usersModel) => {
+  let users = [];
+  usersModel.rows.map(user => {
+    return users.push(FilterUserAndUserCourse(user));
+  })
+  res.status(200).send((users)).end();
 }));
 router.post('/populer', (req, res) => JustPopulerUserAndCourses().then((usersModel) => {
   let users = [];
