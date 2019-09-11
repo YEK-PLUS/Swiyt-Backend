b43774b2e301a7da2817eab1769cb50fb7bd04ea
YEK-PLUS <admin@f4med.com>
2019-09-11 23:19:49 +0300

    added referance endpoint

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import Models from '../models';
+const {
+  AllReferences
+} = Models;
 
 const router = Router();
 router.get('/', (req, res) => {
   res.status(200).send("it's working");
 });
+router.post('/references', (req, res) => AllReferences().then(references => {
+  res.status(200).send(references);
+}));
 
 module.exports = router;
