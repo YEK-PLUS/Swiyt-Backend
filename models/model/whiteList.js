be741c6f21d044d813db92501b91a88b8ded071b
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-21 21:46:09 +0300

    added new model wishlist

---
+++ models/model/whiteList.js
@@ -0,0 +1,15 @@
+import Sequelize from 'sequelize';
+
+module.exports = sequelize.define('wish_lists', {
+  uid: { type: Sequelize.CHAR, primaryKey: true },
+  users_uid: Sequelize.CHAR,
+  lessons_uid: Sequelize.CHAR,
+
+  wish_list: Sequelize.BOOLEAN,
+
+  deleted_at: Sequelize.DATE,
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE,
+}, {
+  underscored: true,
+});
