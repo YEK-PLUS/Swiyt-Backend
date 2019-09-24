71c6308adf8c59532278ed38e53337da75d463e6
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 00:05:11 +0300

    new modern wishlist filter

---
+++ models/filters/other/wishList.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+import FilterUser from '../user/user';
+import FilterCourse from '../course/course';
+
+module.exports = (WishListModel) => {
+  const wishList = WishListModel.toJSON();
+  const filteredWishList = _.pick(wishList, [
+    'uid',
+    'lessons_uid',
+    'users_uid',
+    'wish_list',
+  ]);
+  if (_.has(wishList, 'user')) {
+    filteredWishList.user = FilterUser(WishListModel.user);
+  }
+  if (_.has(wishList, 'lesson')) {
+    filteredWishList.lesson = FilterCourse(WishListModel.lesson);
+  }
+  return filteredWishList;
+};
