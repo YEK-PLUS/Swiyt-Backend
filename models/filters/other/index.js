9f09ade6e022bd91de2844a70c28e3b5b937f17e
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 18:50:53 +0300

    update other filters imp

---
+++ models/filters/other/index.js
@@ -0,0 +1,17 @@
+import FilterImage from './image';
+import FilterWishList from './wishList';
+import FilterComment from './comment';
+import FilterCategory from './category';
+import FilterSubscription from './subscription';
+import FilterSwiytComment from './swiytComment';
+import FilterReferance from './referance';
+
+module.exports = {
+  FilterImage,
+  FilterWishList,
+  FilterComment,
+  FilterCategory,
+  FilterSubscription,
+  FilterSwiytComment,
+  FilterReferance
+};
