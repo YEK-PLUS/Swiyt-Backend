5c9f11b684cfa853bd84cb7dbe1694f439d68da6
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 18:48:01 +0300

    model inc type updated

---
+++ models/includes/index.js
@@ -0,0 +1,19 @@
+import IncludeUser from './user';
+import IncludeSubscription from './subscription';
+import IncludeLesson from './lesson';
+import IncludeCategory from './category';
+import IncludeComment from './comment';
+import IncludeImage from './image';
+import IncludeUserDetail from './userDetails';
+import IncludeWishList from './wishList';
+
+module.exports = {
+  IncludeCategory,
+  IncludeComment,
+  IncludeImage,
+  IncludeLesson,
+  IncludeSubscription,
+  IncludeUser,
+  IncludeUserDetail,
+  IncludeWishList,
+};
