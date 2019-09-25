8c9cb37082140fb707477446dc548c8c066bec62
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 18:44:41 +0300

    added new model setter

---
+++ models/setter/wishlist.js
@@ -0,0 +1,24 @@
+import uuid from 'uuid/v4';
+import WishList from '../model/whiteList';
+
+export default (lessons_uid, users_uid, wish_list) => WishList.findOne({
+  where: {
+    lessons_uid,
+    users_uid,
+  },
+}).then((wishList) => {
+  if (wishList) {
+    return wishList.update({
+      wish_list,
+    });
+  }
+  const uid = uuid();
+
+  const buildWishList = {
+    uid,
+    lessons_uid,
+    users_uid,
+    wish_list,
+  };
+  return WishList.build(buildWishList).save();
+});
