f8ed287a0e84c52807492179a6d416015b3ee912
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 18:49:21 +0300

    updaste course getter

--- models/getter/course.js
+++ models/getter/course.js
@@ -1,48 +1,15 @@
 import Course from '../model/course';
-import User from '../model/user';
-import Subscription from '../model/subscription';
 import * as Includes from '../includes';
 
 const {
-  IncludeCourse, IncludeSubscription, IncludeImage, IncludeUserCourses,
+  IncludeLesson,
 } = Includes;
-export const PopulerCourses = () => Course.findAll({
-  limit: 5,
-  include: IncludeCourse(),
-});
-export const AllPopulerCourses = () => Course.findAll({
-  include: IncludeCourse(),
-});
-export const Subscriptions = (userUid) => Subscription.findAll({
-  where: {
-    user_uid: userUid,
-  },
-  limit: 5,
-  include: IncludeSubscription,
-});
-export const AllSubscriptions = (userUid) => Subscription.findAll({
-  where: {
-    user_uid: userUid,
-  },
-  include: IncludeSubscription,
-});
-export const CourseDetail = (username, course,userUid) => {
-  return User.findOne({
-    where: sequelize.where(
-      sequelize.fn('lower', sequelize.col('username')),
-      sequelize.fn('lower', username),
-    ),
-    include: [
-      {
-        ...IncludeUserCourses(userUid),
-        where: sequelize.where(
-          sequelize.fn('lower', sequelize.col('name')),
-          sequelize.fn('lower', course.replace('-', ' ')),
-        ),
-      },
-      {
-        ...IncludeImage,
-      },
-    ],
-  });
+const JustCourse = (all,props) => {
+  const re_props = typeof all === 'string' ? props : all;
+  const re_all = typeof all !== 'string' ? 'findOne' : all;
+  return Course[re_all](IncludeLesson(re_props));
 }
+
+module.exports = {
+  JustCourse,
+};
