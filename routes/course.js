453dc5c94c6e63ce8ef8e4a5a76fabe727767be0
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 18:42:36 +0300

    update route with modern imp

--- routes/course.js
+++ routes/course.js
@@ -1,50 +1,207 @@
- import { Router } from 'express';
+import { Router } from 'express';
 import auth from '../middlewares/auth';
 import Models from '../models';
 
+
 const {
-  PopulerCourses,
-  AllPopulerCourses,
-  Subscriptions,
-  AllSubscriptions,
-  FilterCourse,
-  FilterSubscription,
-  CourseDetail,
-  FilterUserAndUserCourse,
-  UpdateWishList
+  JustCourse, FilterCourse,FilterSubscription,JustSubsription,UpdateWishList,JustUser,FilterUser
 } = Models;
 const router = Router();
 
-router.post('/populer', (req, res) => PopulerCourses().then((courses) => {
+router.post('/populer', (req, res) => JustCourse('findAll',{
+  including:{
+    userIncluding:true,
+    userOpt:{
+      opt:{
+        asAdmin:true,
+      }
+    },
+    commentIncluding:true,
+    commentOpt:{
+      including:{
+        userIncluding:true
+      }
+    },
+    subscriptionIncluding:true,
+    subscriptionOpt:{
+      including:{
+        userIncluding:true
+      }
+    }
+  }
+}).then((courses) => {
   const a = [];
   courses.map((b) => a.push(FilterCourse(b)));
   return res.status(200).send(a).end();
 }));
-router.post('/populer/all', (req, res) => AllPopulerCourses().then((courses) => {
+router.post('/populer/all', (req, res) => JustCourse('findAll',{
+  including:{
+    userIncluding:true,
+    userOpt:{
+      opt:{
+        asAdmin:true,
+      }
+    },
+    commentIncluding:true,
+    commentOpt:{
+      including:{
+        userIncluding:true
+      }
+    },
+    subscriptionIncluding:true,
+    subscriptionOpt:{
+      including:{
+        userIncluding:true
+      }
+    }
+  }
+}).then((courses) => {
   const a = [];
   courses.map((b) => a.push(FilterCourse(b)));
   return res.status(200).send(a).end();
 }));
-router.post('/subscriptions', auth, (req, res) => Subscriptions(req.user.uid).then((subscriptions) => {
+router.post('/subscriptions', auth, (req, res) => JustSubsription('findAll',{
+  opt:{
+    where:{user_uid: req.user.uid}
+  },
+  including:{
+    lessonIncluding:true,
+    lessonOpt:{
+      including:{
+        userIncluding:true,
+        userOpt:{
+          opt:{
+            asAdmin:true,
+          }
+        },
+      }
+    }
+  }
+}).then((subscriptions) => {
   const a = [];
   subscriptions.map((b) => a.push(FilterSubscription(b)));
   return res.status(200).send(a).end();
 }));
-router.post('/subscriptions/all', auth, (req, res) => AllSubscriptions(req.user.uid).then((subscriptions) => {
+router.post('/subscriptions/all', auth, (req, res) => JustSubsription('findAll',{
+  opt:{
+    where:{user_uid: req.user.uid}
+  },
+  including:{
+    lessonIncluding:true,
+    lessonOpt:{
+      userIncluding:true,
+      userOpt:{
+        opt:{
+          asAdmin:true,
+        }
+      },
+    }
+  }
+}).then((subscriptions) => {
   const a = [];
   subscriptions.map((b) => a.push(FilterSubscription(b)));
   return res.status(200).send(a).end();
 }));
-router.post('/wishList',auth, (req, res) => {
-  let {wishList} = req.body;
+router.post('/wishList', auth, (req, res) => {
+  let { wishList,lessonUid } = req.body;
   wishList = (wishList == 0 || wishList == 1) ? wishList : 0;
-  UpdateWishList(req.body.lessonUid,req.user.uid,wishList).then((result) => {
-    if(!result){
-      return res.status(200).send(key.returns.error).end()
+  UpdateWishList(lessonUid, req.user.uid, wishList).then((result) => {
+    if (!result) {
+      return res.status(200).send(key.returns.error).end();
     }
     return res.status(200).send(key.returns.success).end();
-  })
+  });
 });
-router.post('/', (req, res) => CourseDetail(req.body.user, req.body.course).then((course) => res.status(200).send(FilterUserAndUserCourse(course)).end()));
-router.post('/auth',auth, (req, res) => CourseDetail(req.body.user, req.body.course,req.user.uid).then((course) => res.status(200).send(FilterUserAndUserCourse(course)).end()));
+router.post('/', (req, res) => JustUser({
+  opt:{
+    where:sequelize.where(
+      sequelize.fn('lower', sequelize.col('username')),
+      sequelize.fn('lower', req.body.user),
+    ),
+  },
+  including:{
+    lessonIncluding:true,
+    lessonOpt:{
+      opt:{
+        where:sequelize.where(
+          sequelize.fn('lower', sequelize.col('name')),
+          sequelize.fn('lower', req.body.course.replace('-', ' ')),
+        ),
+      },
+      including:{
+        userIncluding:true,
+        userOpt:{
+          opt:{
+            asAdmin:true,
+          },
+          including:{
+            userDetailIncluding:true
+          }
+        },
+        commentIncluding:true,
+        commentOpt:{
+          including:{
+            userIncluding:true
+          }
+        },
+        subscriptionIncluding:true,
+        subscriptionOpt:{
+          including:{
+            userIncluding:true
+          }
+        }
+      }
+    }
+  }
+}).then((course) => res.status(200).send(FilterUser(course)).end()));
+router.post('/auth',auth, (req, res) => JustUser({
+  opt:{
+    where:sequelize.where(
+      sequelize.fn('lower', sequelize.col('username')),
+      sequelize.fn('lower', req.body.user),
+    ),
+  },
+  including:{
+    lessonIncluding:true,
+    lessonOpt:{
+      opt:{
+        where:sequelize.where(
+          sequelize.fn('lower', sequelize.col('name')),
+          sequelize.fn('lower', req.body.course.replace('-', ' ')),
+        ),
+      },
+      including:{
+        userIncluding:true,
+        userOpt:{
+          opt:{
+            asAdmin:true,
+          },
+          including:{
+            userDetailIncluding:true
+          }
+        },
+        commentIncluding:true,
+        commentOpt:{
+          including:{
+            userIncluding:true
+          }
+        },
+        subscriptionIncluding:true,
+        subscriptionOpt:{
+          including:{
+            userIncluding:true
+          }
+        },
+        wishListIncluding:true,
+        wishListOpt:{
+          opt:{
+            where:{
+              users_uid:req.user.uid
+            }
+          }
+        }
+      }
+    }
+  }
+}).then((course) => res.status(200).send(FilterUser(course)).end()));
 module.exports = router;
