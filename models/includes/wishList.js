54cda0ff873366d6b2c39b491989c018b473c06f
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-25 18:46:18 +0300

    added new wishlist inc model

---
+++ models/includes/wishList.js
@@ -0,0 +1,34 @@
+import Models from '../model';
+import IncludeUser from './user';
+import IncludeLesson from './lesson';
+
+
+const { WhiteList } = Models;
+export default (props) => {
+  const { including, opt } = props || {};
+  const {
+    lessonIncluding,
+    lessonOpt,
+    userIncluding,
+    userOpt,
+  } = including || {};
+  const { where, required } = opt || {};
+  const model = {
+    model: WhiteList,
+    include: [],
+  };
+  if (required) {
+    model.required = true;
+  }
+  if (where) {
+    model.where = where;
+  }
+
+  if (userIncluding || userOpt) {
+    model.include.push(IncludeUser(userOpt));
+  }
+  if (lessonIncluding || lessonOpt) {
+    model.include.push(IncludeLesson(lessonOpt));
+  }
+  return model;
+};
